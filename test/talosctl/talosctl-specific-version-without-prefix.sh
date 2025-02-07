eec171e5c88f1bf86dbe5f9b0851d4829b834361
Kevin Tijssen <kevin.tijssen@gmail.com>
2025-02-07 14:14:45 +0100

    Updating files: 2

--- test/talosctl/talosctl-specific-version-without-prefix.sh
+++ test/talosctl/talosctl-specific-version-without-prefix.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "gum specific version 1.9.3" /bin/bash -c "talosctl version | grep 'v1.9.3'"
+check "talosctl specific version 1.9.3" /bin/bash -c "talosctl version | grep 'v1.9.3'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
--- test/talosctl/test.sh
+++ test/talosctl/test.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "talosctl" talosctl version
+check "talosctl" command -v talosctl
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
