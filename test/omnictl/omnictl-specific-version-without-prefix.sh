addc1a58c47bea8606c51fa1a9e42f9d8db0b394
Kevin Tijssen <kevin.tijssen@gmail.com>
2025-04-08 07:48:39 +0200

    added omnictl tests

--- test/omnictl/omnictl-specific-version-without-prefix.sh
+++ test/omnictl/omnictl-specific-version-without-prefix.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "omnictl specific version 0.47.1" /bin/bash -c "omnictl version | grep 'v0.47.1'"
+check "omnictl specific version 0.47.1" /bin/bash -c "omnictl --version | grep 'v0.47.1'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
