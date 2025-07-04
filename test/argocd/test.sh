937412bc6c67996c3445d2b747f422d57ab1f183
Kevin Tijssen <kevin.tijssen@gmail.com>
2025-07-04 08:59:25 +0200

    fix(argocd) added --client

--- test/argocd/test.sh
+++ test/argocd/test.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "argocd" argocd version
+check "argocd" argocd version --client
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
